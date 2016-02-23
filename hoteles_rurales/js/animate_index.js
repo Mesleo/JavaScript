b21e1f0d6e7b1f6cd0b33899ead3fac8e0300ab0
JaviBP <javifocus88@hotmail.com>
2016-02-24 00:44:28 +0100

    hoteles rurales

---
+++ hoteles_rurales/js/animate_index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Javier Benítez del Pozo on 23/02/2016.
+ */
+
+(function () {
+
+    var altura = screen.availHeight;
+    var alturaCasa = altura/3+'px';
+
+
+    function mostrarDisplay(){
+        $('#principal').fadeIn(500);
+        $('#images-index').css('display', 'none');
+    }
+
+    $(function(){
+        console.log(alturaCasa);
+        $('.casa-1, .casa-2, .casa-3').css('height', alturaCasa);
+        $('.casa-1, .casa-3').animate({
+            'margin-left': '105%'
+        }, 5000, mostrarDisplay);
+        $('.casa-2, .casa-4').animate({
+            'margin-left': '-105%'
+        }, 5000);
+    });
+})();
